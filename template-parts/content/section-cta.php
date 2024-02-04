968859e5b8d4b8d85bf988d0fcdecfae28d41f94
Valery Pronin <pownmymail@gmail.com>
2024-02-05 00:28:21 +0300

    feat: Front CTA

--- template-parts/content/section-cta.php
+++ template-parts/content/section-cta.php
@@ -1,5 +1,23 @@
-<section class="py-24 bg-neutral-900">
-  <h2 class="heading">
-    CTA
-  </h2>
+<section class="py-24 bg-neutral-900 relative">
+
+  <!-- BgImage -->
+  <picture>
+    <source media="(min-width:640px )" srcset="http://zankovets.local/wp-content/uploads/2024/02/Screenshot-2024-02-05-at-00.21.59.png">
+    <img src="http://zankovets.local/wp-content/uploads/2024/02/Screenshot-2024-02-05-at-00.21.59.png" alt="" class="absolute top-0 left-0 w-full h-full object-cover object-center">
+  </picture>
+  <!-- BgOverlay -->
+  <div class="absolute top-0 left-0 w-full h-full bg-neutral-900 bg-opacity-80"></div>
+
+  <div class="wrapper">
+    <div class="box">
+      <!-- Inner -->
+      <div class="h-[340px] grid place-items-center relative">
+        <h2 class="heading mb-12 text-center leading-normal">
+          Boost your Professional Growth
+          <br>With ProCamp
+        </h2>
+        <a href="#" class="button">show dates</a>
+      </div>
+    </div>
+  </div>
 </section>
