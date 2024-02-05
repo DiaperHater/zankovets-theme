2b4d6e17bf02450eaef11c78bde5798b9ee88c77
Valery Pronin <pownmymail@gmail.com>
2024-02-05 18:32:50 +0300

    feat: Full CTA

--- template-parts/content/section-cta.php
+++ template-parts/content/section-cta.php
@@ -1,9 +1,17 @@
+<?php
+
+$image = get_sub_field('image');
+$heading = get_sub_field('heading');
+$link = get_sub_field('link');
+?>
+
+<!-- CTA -->
 <section class="py-24 bg-neutral-900 relative">
 
   <!-- BgImage -->
   <picture>
-    <source media="(min-width:640px )" srcset="http://zankovets.local/wp-content/uploads/2024/02/Screenshot-2024-02-05-at-00.21.59.png">
-    <img src="http://zankovets.local/wp-content/uploads/2024/02/Screenshot-2024-02-05-at-00.21.59.png" alt="" class="absolute top-0 left-0 w-full h-full object-cover object-center">
+    <source media="(min-width:640px )" srcset="<?= $image['sizes']['large'] ?>">
+    <img src="<?= $image['sizes']['medium_large'] ?>" alt="<?= $image['alt'] ?>" class="absolute top-0 left-0 w-full h-full object-cover object-center">
   </picture>
   <!-- BgOverlay -->
   <div class="absolute top-0 left-0 w-full h-full bg-neutral-900 bg-opacity-80"></div>
@@ -11,12 +19,11 @@
   <div class="wrapper">
     <div class="box">
       <!-- Inner -->
-      <div class="h-[340px] grid place-items-center relative">
+      <div class="grid place-items-center relative">
         <h2 class="heading mb-12 text-center leading-normal">
-          Boost your Professional Growth
-          <br>With ProCamp
+          <?= $heading ?>
         </h2>
-        <a href="#" class="button">show dates</a>
+        <a href="<?= $link['url'] ?>" class="button"><?= $link['title'] ?></a>
       </div>
     </div>
   </div>
