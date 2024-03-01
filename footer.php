ad33d24b058bc9f8cc2cb57ad707c10e16b55b86
Valery Pronin <pownmymail@gmail.com>
2024-03-01 22:05:55 +0300

    Add to footer 'Website by Valery Pronin'

--- footer.php
+++ footer.php
@@ -7,42 +7,47 @@
         <!-- Navigation -->
         <div class="w-[920px] max-w-full mx-auto border-b border-b-yellow-300 pb-8 mb-12 md:grid md:grid-cols-8 md:gap-x-6 lg:items-center">
           <!-- Logo -->
-          <a href="<?= home_url() ?>" class="text-4xl text-yellow-300 leading-none font-bold uppercase duration-200 hover:text-yellow-500  block text-center mb-12 md:col-span-2 md:col-start-4 md:mb-0">
+          <a href="<?= home_url() ?>" class="block mb-12 text-4xl font-bold leading-none text-center text-yellow-300 uppercase duration-200 hover:text-yellow-500 md:col-span-2 md:col-start-4 md:mb-0">
             Zankovets
           </a>
           <!-- leftColumn -->
-          <div class="flex flex-col gap-y-6 items-center mb-6 md:col-span-3 md:m-0 md:row-start-1 lg:flex-row lg:gap-x-6 lg: justify-end">
-            <a href="/about" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500  decoration-4 underline-offset-4 hover:underline">
+          <div class="flex flex-col items-center justify-end mb-6 gap-y-6 md:col-span-3 md:m-0 md:row-start-1 lg:flex-row lg:gap-x-6 lg:">
+            <a href="/about" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500 decoration-4 underline-offset-4 hover:underline">
               About
             </a>
-            <a href="/services" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500  decoration-4 underline-offset-4 hover:underline">
+            <a href="/services" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500 decoration-4 underline-offset-4 hover:underline">
               Services
             </a>
-            <a href="/blog" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500  decoration-4 underline-offset-4 hover:underline">
+            <a href="/blog" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500 decoration-4 underline-offset-4 hover:underline">
               Blog
             </a>
           </div>
           <!-- rightColumn -->
-          <div class="flex flex-col gap-y-6 items-center md:col-span-3 md:m-0 lg:flex-row lg:gap-x-6">
-            <a href="/articles" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500  decoration-4 underline-offset-4 hover:underline">
+          <div class="flex flex-col items-center gap-y-6 md:col-span-3 md:m-0 lg:flex-row lg:gap-x-6">
+            <a href="/articles" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500 decoration-4 underline-offset-4 hover:underline">
               Scientific Articles
             </a>
-            <a href="/books" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500  decoration-4 underline-offset-4 hover:underline">
+            <a href="/books" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500 decoration-4 underline-offset-4 hover:underline">
               Books
             </a>
-            <a href="#" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500  decoration-4 underline-offset-4 hover:underline">
+            <a href="#" class="text-base text-yellow-300 uppercase duration-200 hover:text-yellow-500 decoration-4 underline-offset-4 hover:underline">
               Contact
             </a>
           </div>
         </div>
         <!-- Socials -->
-        <div class="flex gap-x-6 justify-center mb-20">
+        <div class="flex justify-center mb-20 gap-x-6">
           <?php get_template_part('template-parts/components/social-icons', null) ?>
         </div>
-        <!-- Sign -->
-        <p class="text-center font-light text-yellow-300">
-          © 2024
-        </p>
+        <div class="flex justify-between font-light text-yellow-300">
+          <!-- Sign -->
+          <p class="">
+            © valdyzankovets.com <?php echo date("Y"); ?>
+          </p>
+          <p>
+            Website by <a href="https://valerypronin.com" class=""><strong>Valery Pronin</strong></a>
+          </p>
+        </div>
       </div>
     </div>
   </footer>
