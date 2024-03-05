fa57393eb25f62b0b65936fef6218c67f9a32cf7
Valery Pronin <pownmymail@gmail.com>
2024-03-05 20:45:46 +0300

    Made excerpt shown on statement in single.php

--- single.php
+++ single.php
@@ -16,28 +16,28 @@ $blocks = parse_blocks($content);
   ) {
   ?>
     <!-- Hero -->
-    <div class="pt-24 pb-12 mb-24 relative">
+    <div class="relative pt-24 pb-12 mb-24">
       <!-- BG -->
-      <img src="<?= the_post_thumbnail_url('medium_large') ?>" alt="" class="absolute top-0 left-0 w-full h-full object-cover object-center">
+      <img src="<?= the_post_thumbnail_url('medium_large') ?>" alt="" class="absolute top-0 left-0 object-cover object-center w-full h-full">
       <!-- BViewverlay -->
-      <div class="absolute top-0 left-0 w-full h-full bg-neutral-900 bg-opacity-70 duration-300 group-hover:backdrop-blur-sm"></div>
+      <div class="absolute top-0 left-0 w-full h-full duration-300 bg-neutral-900 bg-opacity-70 group-hover:backdrop-blur-sm"></div>
       <!-- Inner -->
       <div class="wrapper">
-        <div class="box relative">
+        <div class="relative box">
           <!-- Preheading -->
-          <p class="mb-4 text-white font-bold text-lxl text-right">
+          <p class="mb-4 font-bold text-right text-white text-lxl">
             <?= get_the_date() ?>
           </p>
-          <p class="mb-6 text-white text-2xl text-right">
+          <p class="mb-6 text-2xl text-right text-white">
             <a href="/about">by: Vlady Zankovets</a>
           </p>
           <!-- Heading -->
-          <h2 class="heading text-yellow-300 text-center max-w-xl mx-auto pb-12">
+          <h2 class="max-w-xl pb-12 mx-auto text-center text-yellow-300 heading">
             <?= get_the_title() ?>
           </h2>
           <!-- Subheading -->
-          <p class="text-2xl tracking-wide text-yellow-300 text-center max-w-2xl mx-auto">
-            <?= get_the_excerpt() ?>
+          <p class="max-w-2xl mx-auto text-2xl tracking-wide text-center text-yellow-300">
+            <?= has_excerpt() ? get_the_excerpt() : '&nbsp;' ?>
           </p>
         </div>
       </div>
@@ -50,7 +50,7 @@ $blocks = parse_blocks($content);
     <div class="box-sm">
 
       <!-- Content -->
-      <div class="prose prose-2xl mx-auto">
+      <div class="mx-auto prose prose-2xl">
         <?php
         foreach ($blocks as $block) {
           if ("core/shortcode" == $block['blockName']) {
